5b5d065875e5179197d00dced1773a36dfaacf60
YaroShkvorets <shkvorets@gmail.com>
2021-02-11 18:00:20 -0500

    fixed evodex calculations

--- evodex.hpp
+++ evodex.hpp
@@ -44,112 +44,56 @@ namespace evodex {
 
 
     /**
-     * ## STATIC `get_fee`
-     *
-     * Get total fee
-     *
-     * ### returns
-     *
-     * - `{uint8_t}` - total fee (trade + protocol)
-     *
-     * ### example
-     *
-     * ```c++
-     * const uint8_t fee = evodex::get_fee();
-     * // => 30
-     * ```
-     */
-    static uint8_t get_fee()
-    {
-        //all pools have 30 bips fee, if that changes - we could lookup it up in stat table
-        return 30;
-    }
-
-    /**
-     * ## STATIC `get_reserves`
-     *
-     * Get reserves for a pair
-     *
-     * ### params
-     *
-     * - `{string} pair_id` - pair id
-     * - `{symbol} sort` - sort by symbol (reserve0 will be first item in pair)
-     *
-     * ### returns
-     *
-     * - `{pair<asset, asset>}` - pair of reserve assets
-     *
-     * ### example
-     *
-     * ```c++
-     * const string pair_id = "EOSUSDT";
-     * const symbol sort = symbol{"EOS", 4};
-     *
-     * const auto [reserve0, reserve1] = evodex::get_reserves( pair_id, sort );
-     * // reserve0 => "4638.5353 EOS"
-     * // reserve1 => "13614.8381 USDT"
-     * ```
-     */
-    static pair<asset, asset> get_reserves( const string& pair_id, const symbol sort )
-    {
-        // table
-        symbol_code evo_symcode {pair_id};
-        check(evo_symcode.is_valid(), "EvodexLibrary: Invalid pair_id");
-
-        evodex::stat _pools( evodex::code, evo_symcode.raw() );
-
-        check(_pools.begin() != _pools.end(), "EvodexLibrary: Invalid Evodex symbol");
-
-        auto pool = *_pools.begin();
-
-        eosio::check( pool.pool1.quantity.symbol == sort || pool.pool2.quantity.symbol == sort, "EvodexLibrary: sort symbol doesn't match");
-
-        return sort == pool.pool1.quantity.symbol ?
-            pair<asset, asset>{ pool.pool1.quantity, pool.pool2.quantity } :
-            pair<asset, asset>{ pool.pool2.quantity, pool.pool1.quantity };
-    }
-
-    /**
      * ## STATIC `get_amount_out`
      *
-     * Given an input amount of an asset and pair reserves, returns the output amount of the other asset
-     * source: https://github.com/EOSArgentina/evolutiondex/blob/master/evolutiondex.cpp#L165
+     * Given an input amount of an asset and pair id, returns the calculated return
      *
      * ### params
      *
-     * - `{uint64_t} amount_in` - amount input
-     * - `{uint64_t} reserve_in` - reserve input
-     * - `{uint64_t} reserve_out` - reserve output
-     * - `{uint64_t} fee` - trading fee (pips 1/10000)
+     * - `{asset} in` - input amount
+     * - `{symbol} out_sym` - out symbol
+     * - `{string} pair_id` - pair_id
      *
      * ### example
      *
      * ```c++
      * // Inputs
-     * const uint64_t amount_in = 10000;
-     * const uint64_t reserve_in = 45851931234;
-     * const uint64_t reserve_out = 46851931234;
-     * const uint64_t fee = 5;
+     * const asset in = asset { 10000, "EOS" };
+     * const symbol out_sym = symbol {"USDT,4"};
+     * const string pair_id = "EOSUSDT"
      *
      * // Calculation
-     * const uint64_t amount_out = evodex::get_amount_out( amount_in, reserve_in, reserve_out, fee );
-     * // => 9996
+     * const asset out = evodex::get_amount_out( in, out_sym, pair_id );
+     * // => 3.3996 USDT
      * ```
      */
-    static uint64_t get_amount_out( const uint64_t amount_in, const uint64_t reserve_in, const uint64_t reserve_out,  const uint64_t fee )
+
+    static asset get_amount_out( const asset in, symbol out_sym, const string& pair_id )
     {
-        // checks
-        eosio::check(amount_in > 0, "sx.evodex: INSUFFICIENT_INPUT_AMOUNT");
-        eosio::check(reserve_in > 0 && reserve_out > 0, "sx.evodex: INSUFFICIENT_LIQUIDITY");
+         // checks
+        eosio::check(in.amount > 0, "EvodexLibrary: INSUFFICIENT_INPUT_AMOUNT");
 
-        int128_t am_in = -int128_t(amount_in);
-        int128_t res_in = (int128_t)reserve_in + (int128_t)amount_in;
+        symbol_code liq_symcode {pair_id};
+        check(liq_symcode.is_valid(), "EvodexLibrary: Invalid pair_id");
 
-        int128_t tmp = am_in * reserve_out / res_in;
-        int128_t tmp_fee = (-tmp * fee + 9999)/10000;
-        tmp += tmp_fee;
+        evodex::stat _stat( evodex::code, liq_symcode.raw() );
+        auto row = _stat.get(liq_symcode.raw(), "EvodexLibrary: Invalid Evodex symbol");
 
-        return -tmp;
+        if(row.pool1.quantity.symbol != in.symbol) std::swap(row.pool1, row.pool2);
+        check( row.pool1.quantity.symbol == in.symbol && row.pool2.quantity.symbol == out_sym, "EvodexLibrary: invalid pair");
+        check( row.pool1.quantity.amount > 0 && row.pool2.quantity.amount > 0, "EvodexLibrary: INSUFFICIENT_LIQUIDITY");
+
+        auto am_in = in.amount;
+        auto res_in = row.pool1.quantity.amount + in.amount;
+        auto res_out = row.pool2.quantity.amount;
+
+        auto am_out = am_in * res_out / res_in;
+        auto fee = (9999 + am_out * row.fee)/10000;
+        am_out -= fee;
+
+        check(am_out > 0, "EvodexLibrary: invalid return");
+
+        return asset { am_out, out_sym };
     }
 
 }
